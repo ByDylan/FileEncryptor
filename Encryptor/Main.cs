c12c200359ebaa2ab3e6da5c5bf42516cf8c4c7d
apexMadeIt <60795070+apexMadeIt@users.noreply.github.com>
2020-02-22 01:14:11 -0500

    Add files via upload

---
+++ Encryptor/Main.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Threading;
+
+namespace Encryptor
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Thread.Sleep(2300);
+            Console.Beep();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog FDG = new OpenFileDialog();
+            FDG.Filter ="All Files|*.*";
+            if (FDG.ShowDialog() == DialogResult.OK)
+            {
+                string path = FDG.FileName.ToString();
+                textBox1.Text = path;
+            }
+        }
+
+        public byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            byte[] saltBytes = new byte[] { 1, 2, 38, 6, 4, 5, 6, 3, 4, 9, 6, 34, 4, 9, 4, 3, 3, 46, 54, 64, 5, 31, 24, 65, 1, 63, 4, 56, 46, 3, 7, 3 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged()) 
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] decrypted = File.ReadAllBytes(textBox1.Text);
+            byte[] encrypted = AES_Encrypt(decrypted, Encoding.UTF8.GetBytes("WET43QIOahijbnfjkasiHIOFOI"));
+            string path = textBox1.Text.Substring(0, textBox1.Text.Length) + "encrypt";
+            File.WriteAllBytes(path, encrypted);
+            Thread.Sleep(2300);
+            MessageBox.Show("Success");
+            Close();
+        }
+    }
+
+}
---
+++ Encryptor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Encryptor")]
+[assembly: AssemblyDescription("File Encrypter")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("dylan.#7341")]
+[assembly: AssemblyProduct("Encryptor")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5d66bee-a316-4a7b-9c9b-b93bb16c102d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
